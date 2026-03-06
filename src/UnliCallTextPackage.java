8d0830e0c2b1c4429414bce74cef4a992eb559cb
Jermaine Carl Miranda <jermaine.carlm@gmail.com>
2026-03-07 01:32:51 +0800

    Update UnliCallTextPackage.java

--- src/UnliCallTextPackage.java
+++ src/UnliCallTextPackage.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 public class UnliCallTextPackage implements UnliCallsTextOffer {
+    private static Map<String, String> unliMap = new HashMap<>();
+
+    static {
+        unliMap.put("Smart", "not offer any free calls or texts, and you will be charged per use.");
+        unliMap.put("Globe", "unlimited calls and texts within network. Others charged extra.");
+        unliMap.put("Ditto", "unlimited calls and texts to all networks within the country.");
+    }
+
     @Override
     public String showUnliCallsTextOffer(String telcoName, boolean unliCallText) {
-        if (telcoName.equals("Smart")) return "not offer any free calls or texts, and you will be charged per use.";
-        if (telcoName.equals("Globe")) return "unlimited calls and texts within network. Others charged extra.";
-        return "unlimited calls and texts to all networks within the country.";
+        return unliMap.get(telcoName);
     }
 }
